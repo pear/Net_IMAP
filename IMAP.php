0869e163ade9a26792a42b6fb4ef29853243956f
Sebastian Ebling <hudeldudel@php.net>
2008-02-06 23:55:11 +0000

    fix for a bug in the creation of the mail-structure. MULTIPART/RELATED parts where created as structure nested too deep, comared to the structure used by the IMAP-server, when accessing the parts in question directly. Thanks to Klaus Leithoff!
    git-svn-id: https://svn.php.net/repository/pear/packages/Net_IMAP/trunk@252431 c90b9560-bf6c-de11-be94-00142212c4b1

--- IMAP.php
+++ IMAP.php
@@ -581,7 +581,9 @@ class Net_IMAP extends Net_IMAPProtocol {
      */
     function _parseStructureMultipartArray($_structure, &$_mimeParts, $_partID, $_parentIsMessage = false) 
     {
-        // a multipart/mixed, multipart/report or multipart/alternative get's no own partid, if the parent is message/rfc822
+        // a multipart/mixed, multipart/report or multipart/alternative 
+        // or multipart/related get's no own partid, if the parent 
+        // is message/rfc822
         if ($_parentIsMessage == true && is_array($_structure[0])) {
             foreach ($_structure as $structurePart) {
                 if (!is_array($structurePart)) {
@@ -589,7 +591,7 @@ class Net_IMAP extends Net_IMAPProtocol {
                     break;
                 }
             }
-            if ($subType == 'mixed' || $subType == 'report' || $subType == 'alternative') {
+            if ($subType == 'mixed' || $subType == 'report' || $subType == 'alternative' || $subType == 'related') {
                 $_partID = substr($_partID, 0, strrpos($_partID, '.'));
             }
         }
